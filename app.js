c1931648540eaf28eab69e67586d28f368b826ca
AhmedGamal73 <malokokha2@gmail.com>
2023-03-11 22:23:39 +0200

    add the first feature (adding todo)

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-const sayHai= "Hai";
+const todoEl = document.getElementById('todo_title');
+const todosCon = document.getElementById('todos_container');
+const btn = document.getElementById('todo_btn');
+// todoEl.addEventListener('keydown', e => {
+// 	console.log(e.value) 
+// })
 
-console.log(sayHai)
+const displayTodo = () => {
+	const todo = document.createElement('H1');
+	todo.innerHTML = todoEl.value;
+	todosCon.appendChild(todo)
+}
+
+
+const todoTitle = btn.addEventListener('click', (e) => {
+	e.preventDefault();
+	if (todoEl.value !== '') { // check if there's text in the field
+ 		displayTodo();
+		todoEl.value = "";
+	};
+});
+
+const data = {
+	id: Math.random(),
+	title: todoEl.value
+}
+
+console.log(data.title)
+
+
